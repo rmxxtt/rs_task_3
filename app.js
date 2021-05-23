dae42a327386164c5f8732804c250f9ea3bff5b1
rmxxtt <62797019+rmxxtt@users.noreply.github.com>
2021-05-23 23:09:57 +0700

    feat: add db

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const game = require('./controllers/gamecontroller');
 
 db.sync();
 //db.sync.database.
-app.use(require('body-parser'));
+app.use(express.json);
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
